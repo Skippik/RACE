8e6eea45d4c6d7258f2f7c2398e6720499ecfb7d
Evgen <Skippik1111@gmail.com>
2021-06-23 13:36:38 +0300

    dz2_full

--- script/main.js
+++ script/main.js
@@ -22,14 +22,21 @@ const keys = {
 
 const setting = {
     start: false,
-    score: false,
-    speed: 3
+    score: 0,
+    speed: 3,
+    traffic: 3
 };
+
+function qetQuantityElements (heightElement) {
+    return document.documentElement.clientHeight / heightElement +1;
+}
+
+
 
 
 function startGame () {
     start.classList.add('hide');
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < qetQuantityElements(100); i++) {
         const line = document.createElement('div');
         line.classList.add('line');
         line.style.top = (i * 100) + 'px';
@@ -37,6 +44,16 @@ function startGame () {
         gameArea.appendChild(line);
     }
 
+
+    for (let i = 0; i < qetQuantityElements(100 * setting.traffic); i++) {
+        const enemy = document.createElement('div');
+        enemy.classList.add('enemy');
+        enemy.y =  -100 * setting.traffic * (i + 1);
+        enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+        enemy.style.top = enemy.y + 'px';
+        enemy.style.background = 'transparent url(\'../image/enemy.png\') center / cover no-repeat;';
+        gameArea.appendChild(enemy);
+    }
 
 
 
@@ -50,6 +67,7 @@ function startGame () {
 function playGame() {
     if (setting.start === true){
         moveRoad();
+        moveEnemy();
         if (keys.ArrowLeft && setting.x > 0) {
             setting.x -= setting.speed;
         }
@@ -87,8 +105,20 @@ function moveRoad () {
         line.y += setting.speed;
         line.style.top = line.y + 'px';
 
-        if (line.y > document.documentElement.clientHeight) {
+        if (line.y >= document.documentElement.clientHeight) {
             line.y = -120;
         }
+    });
+}
+
+function moveEnemy () {
+    let enemy = document.querySelectorAll('.enemy');
+    enemy.forEach(function(item) {
+        item.y += setting.speed / 2;
+        item.style.top = item.y + 'px';
+        if (item.y >= document.documentElement.clientHeight) {
+            item.y = -100 * setting.traffic;
+            item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+        }    
     });
 }
